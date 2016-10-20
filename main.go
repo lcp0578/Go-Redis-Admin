71f2576b400aa4128a2583e2f037a50252be50c9
李春鹏 <lcp0578@gmail.com>
2016-10-20 22:37:29 +0800

    add html resource

--- main.go
+++ main.go
@@ -21,9 +21,13 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "Hello, I'm coming...") //这个写入到w的是输出到客户端的
 }
 func main() {
-	http.HandleFunc("/", sayhelloName)       //设置访问的路由
+	http.HandleFunc("/", sayhelloName) //设置访问的路由
+	http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+func login(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("login"))
 }

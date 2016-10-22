b27291667694b31066f7708edc5cfb4cb46691a8
mashupeng <mashupeng@outlook.com>
2016-10-22 13:52:19 +0800

    add template package  and set view

--- main.go
+++ main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"strings"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       //解析参数，默认是不会解析的
-	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	w.Write([]byte("Hello, I'm coming..."))
-	//fmt.Fprintf(w, "Hello, I'm coming...") //这个写入到w的是输出到客户端的
+func inti() {
+
 }
+
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", sayhelloName) //设置访问的路由
 	http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
@@ -28,6 +19,26 @@ func main() {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	//	r.ParseForm()                //解析参数，默认是不会解析的
+	//	fmt.Println("form:", r.Form) //这些信息是输出到服务器端的打印信息
+	//	fmt.Println("path", r.URL.Path)
+	//	fmt.Println("scheme", r.URL.Scheme)
+	//	fmt.Println("r.Form[\"test\"]", r.Form["test"])
+	//	for k, v := range r.Form {
+	//		fmt.Println("key:", k)
+	//		fmt.Println("val:", strings.Join(v, ""))
+	//	}
+	log.Println("/")
+	t, _ := template.ParseFiles("views/index.html")
+	t.Execute(w, nil)
+	//w.Write([]byte("Hello, I'm coming..."))
+	//fmt.Fprintf(w, "Hello, I'm coming...") //这个写入到w的是输出到客户端的
+}
+
 func login(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("login"))
+	log.Println("/login")
+	t, _ := template.ParseFiles("views/login.html")
+	t.Execute(w, nil)
+	//w.Write([]byte("login"))
 }
---
+++ rediserver/rediserver.go
@@ -0,0 +1,23 @@
+package rediserver
+
+import (
+	"fmt"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+func Connect() {
+	conn, err := redis.Dial("tcp", "127.0.0.1:6379")
+	if err != nil {
+		fmt.Println("connect error:", err.Error())
+	} else {
+		fmt.Println("connect redis success")
+	}
+	r, err_do := conn.Do("set", "age", 24)
+	if err_do != nil {
+		fmt.Println("do error:", err.Error())
+	} else {
+		fmt.Println(r)
+	}
+	//conn.Close()
+}

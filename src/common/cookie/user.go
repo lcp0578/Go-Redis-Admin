de280f92bad8cb56bc0084785399034a32a92e8a
李春鹏 <lcp0578@gmail.com>
2017-01-10 23:57:13 +0800

    cookie user

--- src/common/cookie/user.go
+++ src/common/cookie/user.go
@@ -3,16 +3,21 @@ package cookie
 import (
 	"Go-Redis-Admin/app/config"
 	"Go-Redis-Admin/src/common/crypto"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 /**
  *   获取用户的信息
  **/
 func GetUserInfo(req *http.Request) (int32, string) {
-	var auth = Get(req, "gra_")
-	log.Println("cipherText", cipherText)
-	plainTextCopy, err := crypto.AesDecode(cipherText, keyText)
+	var auth = Get(req, "gra_auth")
+	log.Println("cipherText", auth)
+	keyText := config.UserAesKey
+	plainTextCopy, err := crypto.AesDecode(auth, keyText)
 	log.Println("plainTextCopy", plainTextCopy)
+	return 0, auth
 }
 
 /**
@@ -28,7 +33,7 @@ func SetUserInfo(w http.ResponseWriter, userId int32, username string) (bool, er
 	if err != nil {
 		return false, err
 	}
-	cookie.Set(w, "gra_username", username, "/", 8600)
-	cookie.Set(w, "gra_auth", cipherText, "/", 8600)
+	Set(w, "gra_username", username, "/", 8600)
+	Set(w, "gra_auth", cipherText, "/", 8600)
 	return true, nil
 }
--- web/static/js/login.js
+++ web/static/js/login.js
@@ -3,6 +3,9 @@ $(function(){
 		$(this).attr("src", "/api/v1/reloadcaptcha?t=" + Date.parse(new Date()));
 	});
 	$('.btn-submit').on('click', function(){
-		
+		var username = $('#username').val();
+		var password = $('#password').val();
+		var captcha = $('#captcha').val();
+		$.post('');
 	});
 })

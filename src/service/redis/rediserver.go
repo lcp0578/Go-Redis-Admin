9ce863a628c25181be2941d61754421bf3384ffe
ChiefMage <mashupeng@outlook.com>
2017-01-13 16:41:24 +0800

    modify redisapi

--- src/service/redis/rediserver.go
+++ src/service/redis/rediserver.go
@@ -4,17 +4,34 @@ import (
 	"fmt"
 	"github.com/garyburd/redigo/redis"
 )
-type redisapi struct{
 
+type redisapi struct {
+	redisconn redis.Conn
 }
-func (this *redisapi) Connect(network string,addredd string) {
+
+func (re *redisapi) Connect(network string, addredd string) {
 	conn, err := redis.Dial(network, addredd)
 	if err != nil {
 		fmt.Println("connect error:", err.Error())
 	} else {
 		fmt.Println("connect redis success")
 	}
-	c:=NewConnection(conn)
-	_=c
+	re.redisconn = conn
+	//c:=NewConnection(conn)
 	//conn.Close()
+}
+func (re *redisapi) ping() {
+	err := redis.PubSubConn{}.Ping("test")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
+func (re *redisapi) Get(args ...interface{}){
+	reply,err:=re.redisconn.Do("GET",args)
+	if err!=nil{
+		fmt.Println(err.Error())
+	}else {
+		fmt.Println(reply)
+	}
+
 }
